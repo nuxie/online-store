e7676061ce2e3e7b45f5d9bd993f9fa4a14145e2
nuxie <zkalinowska96@icloud.com>
2020-04-25 01:08:13 +0200

    Add category module (controller + routing)

---
+++ app/controllers/CategoryController.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+@Singleton
+class CategoryController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def create = Action {
+    Ok("Create category")
+  }
+
+  def createHandle(id: Int) = Action {
+    Ok("Handle create category number " + id)
+  }
+
+  def list = Action {
+    Ok("All categories")
+  }
+
+  def details(id: Int) = Action {
+    Ok("Details of category number " + id)
+  }
+
+  def delete(id: Int) = Action {
+    Ok("Delete category number " + id)
+  }
+
+  def update(id: Int) = Action {
+    Ok("Update category number " + id)
+  }
+
+  def updateHandle = Action {
+    Ok("Handle update category")
+  }
+
+}
