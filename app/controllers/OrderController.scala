0354937f2f3b8733ce4fc731fe2843183b5c29ed
nuxie <zkalinowska96@icloud.com>
2020-04-25 01:10:00 +0200

    Add order module (controller + routing)

---
+++ app/controllers/OrderController.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+@Singleton
+class OrderController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def create = Action {
+    Ok("Create order")
+  }
+
+  def createHandle(id: Int) = Action {
+    Ok("Handle create order number " + id)
+  }
+
+  def list = Action {
+    Ok("All orders")
+  }
+
+  def details(id: Int) = Action {
+    Ok("Details of order number " + id)
+  }
+
+  def delete(id: Int) = Action {
+    Ok("Delete order number " + id)
+  }
+
+  def update(id: Int) = Action {
+    Ok("Update order number " + id)
+  }
+
+  def updateHandle = Action {
+    Ok("Handle update order")
+  }
+
+}
