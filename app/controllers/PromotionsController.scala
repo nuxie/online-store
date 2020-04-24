ddc50153058a9adce7f38403414f826b2a17b0bd
nuxie <zkalinowska96@icloud.com>
2020-04-25 01:11:26 +0200

    Add promotion module (controller + routing)

---
+++ app/controllers/PromotionsController.scala
@@ -0,0 +1,33 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+@Singleton
+class PromotionsController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def create = Action {
+    Ok("Create promotion")
+  }
+
+  def createHandle(id: Int) = Action {
+    Ok("Handle create promotion number " + id)
+  }
+
+  def list = Action {
+    Ok("All promotions")
+  }
+
+  def details(id: Int) = Action {
+    Ok("Details of promotion number " + id)
+  }
+
+  def update(id: Int) = Action {
+    Ok("Update promotion number " + id)
+  }
+
+  def updateHandle = Action {
+    Ok("Handle update promotion")
+  }
+
+}
