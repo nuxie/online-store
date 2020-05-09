e102ab66c7526ec27d5b25033b646c4f6c98c6d6
nuxie <zkalinowska96@icloud.com>
2020-05-09 11:50:05 +0200

    Refactor wishlist; add id to show method

--- app/controllers/WishlistController.scala
+++ app/controllers/WishlistController.scala
@@ -6,19 +6,16 @@ import play.api.mvc._
 @Singleton
 class WishlistController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  // one wishlist per customer; I don't think forms are necessary here?
-  // I will update in the future if the logic changes
-
-  def show = Action {
-    Ok("Showing wishlist")
+  def show(id: Int) = Action {
+    Ok("Showing wishlist id: " + id)
   }
 
   def addProduct(id: Int) = Action {
-    Ok("Adding to cart; product id:" + id)
+    Ok("Adding to wishlist; product id:" + id)
   }
 
   def removeProduct(id: Int) = Action {
-    Ok("Removing from cart; product id:" + id)
+    Ok("Removing from wishlist; product id:" + id)
   }
 
 }
