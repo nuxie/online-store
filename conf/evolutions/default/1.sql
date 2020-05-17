7304e1be3012da717b3ee6a7d8cf8066c5ecbc76
nuxie <zkalinowska96@icloud.com>
2020-05-17 17:14:17 +0200

    Refactor table schemas & fix bugs

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,117 @@
+-- !Ups
+
+CREATE TABLE categories
+(
+    id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    UNIQUE (name)
+);
+
+CREATE TABLE products
+(
+    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name        VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    category_id    INT     NOT NULL,
+    price       REAL    NOT NULL,
+    FOREIGN KEY (category_id) references categories (id)
+);
+
+CREATE TABLE users
+(
+    id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name       VARCHAR NOT NULL,
+    e_mail     VARCHAR NOT NULL,
+    tax_number INT
+);
+
+CREATE TABLE carts_products
+(
+    id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    cart_id    INT     NOT NULL,
+    product_id INT     NOT NULL,
+    quantity   INT     NOT NULL,
+    FOREIGN KEY (product_id) references products (id)
+);
+
+CREATE TABLE carts
+(
+    id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    user_id    INT     NOT NULL,
+    FOREIGN KEY (user_id) references users (id),
+    UNIQUE (user_id)
+);
+
+CREATE TABLE orders_products
+(
+    id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    order_id    INT     NOT NULL,
+    product_id INT     NOT NULL,
+    quantity   INT     NOT NULL,
+    FOREIGN KEY (product_id) references products (id),
+    FOREIGN KEY (order_id) references orders (id)
+);
+
+CREATE TABLE orders
+(
+    id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    user_id    INT     NOT NULL,
+    FOREIGN KEY (user_id) references users (id)
+);
+
+CREATE TABLE invoices
+(
+    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    order_id    INT NOT NULL,
+    payment_due VARCHAR NOT NULL,
+    FOREIGN KEY (order_id) references orders (id)
+);
+
+CREATE TABLE promotions
+(
+    id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name            VARCHAR NOT NULL,
+    flag_active     INT     NOT NULL,
+    product_id      INT     NOT NULL,
+    percentage_sale INT     NOT NULL,
+    FOREIGN KEY (product_id) references products (id)
+);
+
+CREATE TABLE reviews
+(
+    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    product_id  INT     NOT NULL,
+    description VARCHAR NOT NULL,
+    FOREIGN KEY (product_id) references products (id)
+);
+
+CREATE TABLE stock
+(
+    id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    product_id INT     NOT NULL,
+    quantity   INT     NOT NULL,
+    FOREIGN KEY (product_id) references products (id),
+    UNIQUE (product_id)
+);
+
+CREATE TABLE wishlists
+(
+    id         INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    user_id    INT     NOT NULL,
+    product_id INT     NOT NULL,
+    FOREIGN KEY (user_id) references users (id),
+    FOREIGN KEY (product_id) references products (id)
+);
+
+-- !Downs
+
+DROP TABLE carts;
+DROP TABLE categories;
+DROP TABLE users;
+DROP TABLE invoices;
+DROP TABLE orders;
+DROP TABLE products;
+DROP TABLE promotions;
+DROP TABLE reviews;
+DROP TABLE stock;
+DROP TABLE wishlists;
---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,59 @@
+-- !Ups
+
+INSERT INTO carts (id, user_id)
+VALUES (1, 1),
+       (2, 2);
+
+INSERT INTO carts_products (id, cart_id, product_id, quantity)
+VALUES (1, 1, 1, 2),
+       (2, 1, 2, 1);
+
+INSERT INTO categories (id, name)
+VALUES (1, 'A'),
+       (2, 'B');
+
+INSERT INTO users (id, name, e_mail, tax_number)
+VALUES (1, 'userA', 'A@email.com', 654321),
+       (2, 'userB', 'B@email.com', 123456);
+
+INSERT INTO invoices (id, order_id, payment_due)
+VALUES (1, 1, '24-05-2020'),
+       (2, 3, '30-05-2020');
+
+INSERT INTO orders (id, user_id)
+VALUES (1, 2),
+       (2, 1),
+       (3, 2);
+
+INSERT INTO orders_products (id, order_id, product_id, quantity)
+VALUES (1, 1, 1, 1),
+       (2, 1, 2, 2),
+       (3, 2, 1, 1),
+       (4, 3, 2, 3),
+       (5, 3, 1, 2),
+       (6, 3, 3, 4);
+
+
+INSERT INTO products (id, name, description, category_id, price)
+VALUES (1, 'Product1', 'Desc1', 1, 9.99),
+       (2, 'Product2', 'Desc2', 2, 17.12),
+       (3, 'Product3', 'Desc3', 1, 150.00);
+
+INSERT INTO promotions (id, name, flag_active, product_id, percentage_sale)
+VALUES (1, 'promo_act_1', 1, 19.99, 50),
+       (2, 'promo_inact_2', 0, 2, 20);
+
+INSERT INTO reviews (id, product_id, description)
+VALUES (1, 1, 'bad'),
+       (2, 1, 'good'),
+       (3, 2, 'so-so');
+
+INSERT INTO stock (id, product_id, quantity)
+VALUES (1, 1, 10),
+       (2, 3, 15),
+       (3, 2, 2);
+
+INSERT INTO wishlists (id, user_id, product_id)
+VALUES (1, 1, 1),
+       (2, 1, 2),
+       (3, 2, 3);
