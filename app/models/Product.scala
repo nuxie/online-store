a6d06cecfbc729be36aa7e0e889669ebafd33abd
nuxie <zkalinowska96@icloud.com>
2020-05-17 17:31:48 +0200

    Add Product model

---
+++ app/models/Product.scala
@@ -0,0 +1,10 @@
+package models
+
+import play.api.libs.json.Json
+import play.api.libs.json.OFormat
+
+case class Product(id: Int, name: String, description: String, category_id: Int, price: Float)
+
+object Product {
+  implicit val productFormat: OFormat[Product] = Json.format[Product]
+}
