828984a279e473f011f4ee8301af08963831d663
nuxie <zkalinowska96@icloud.com>
2020-05-18 00:13:30 +0200

    Refactor Order model

--- app/models/Order.scala
+++ app/models/Order.scala
@@ -3,7 +3,7 @@ package models
 import play.api.libs.json.Json
 import play.api.libs.json.OFormat
 
-case class Order(id: Int, product_id: Int, user_id: Int, quantity: Int)
+case class Order(id: Int, user_id: Int)
 
 object Order {
   implicit val orderFormat: OFormat[Order] = Json.format[Order]
