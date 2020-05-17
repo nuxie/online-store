3fcf5e5a0554ab26f41bebee4df11561847f329a
nuxie <zkalinowska96@icloud.com>
2020-05-17 22:11:26 +0200

    Modify User model - tax number is optional

--- app/models/User.scala
+++ app/models/User.scala
@@ -3,7 +3,7 @@ package models
 import play.api.libs.json.Json
 import play.api.libs.json.OFormat
 
-case class User(id: Int, name: String, e_mail: String, tax_number: Int)
+case class User(id: Int, name: String, e_mail: String, tax_number: Option[Int])
 
 object User {
   implicit val userFormat: OFormat[User] = Json.format[User]
