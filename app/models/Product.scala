f4d55f8d63739b309d5d8fe8e37b29d0a4da6684
nuxie <zkalinowska96@icloud.com>
2020-05-18 01:04:31 +0200

    Refactor price - Float->Long

--- app/models/Product.scala
+++ app/models/Product.scala
@@ -3,7 +3,7 @@ package models
 import play.api.libs.json.Json
 import play.api.libs.json.OFormat
 
-case class Product(id: Int, name: String, description: String, category_id: Int, price: Float)
+case class Product(id: Int, name: String, description: String, category_id: Int, price: Long)
 
 object Product {
   implicit val productFormat: OFormat[Product] = Json.format[Product]
