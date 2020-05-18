ececcc30a34d6245723219c939d42e2938f5746f
nuxie <zkalinowska96@icloud.com>
2020-05-18 02:10:04 +0200

    Implement Review controller methods

--- app/controllers/ReviewController.scala
+++ app/controllers/ReviewController.scala
@@ -1,37 +1,89 @@
 package controllers
 
 import javax.inject._
+import models.{Review, ReviewRepository}
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
 
+import scala.concurrent.{ExecutionContext, Future}
+
 @Singleton
-class ReviewController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ReviewController @Inject()(cc: MessagesControllerComponents, reviewRepo: ReviewRepository)
+                                 (implicit ec: ExecutionContext) extends MessagesAbstractController(cc) {
 
-  def create = Action {
-    Ok("Create review")
+  val reviewForm: Form[CreateReviewForm] = Form {
+    mapping(
+      "product_id" -> number,
+      "description" -> nonEmptyText
+    )(CreateReviewForm.apply)(CreateReviewForm.unapply)
+  }
+  val updateReviewForm: Form[UpdateReviewForm] = Form {
+    mapping(
+      "id" -> number,
+      "product_id" -> number,
+      "description" -> nonEmptyText
+    )(UpdateReviewForm.apply)(UpdateReviewForm.unapply)
   }
 
-  def createHandle = Action {
-    Ok("Handle create review")
+  def create: Action[AnyContent] = Action { implicit request =>
+    Ok(views.html.review.add(reviewForm))
   }
 
-  def list = Action {
-    Ok("All reviews")
+  def createHandle: Action[AnyContent] = Action.async { implicit request =>
+    reviewForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest(views.html.review.add(errorForm))
+        )
+      },
+      review => {
+        reviewRepo.create(review.product_id, review.description).map { _ =>
+          Redirect(routes.ReviewController.create()).flashing("success" -> "Review created")
+        }
+      }
+    )
   }
 
-  def details(id: Int) = Action {
-    Ok("Details of review number " + id)
+  def list: Action[AnyContent] = Action.async { implicit request =>
+    reviewRepo.list().map(r => Ok(views.html.review.list(r)))
   }
 
-  def delete(id: Int) = Action {
-    Ok("Delete review number " + id)
+  def details(id: Int): Action[AnyContent] = Action.async { implicit request =>
+    val rev: Future[Option[Review]] = reviewRepo.details(id)
+    rev.map {
+      case Some(r) => Ok(views.html.review.details(r))
+      case None => Redirect("/reviews/all")
+    }
   }
 
-  def update(id: Int) = Action {
-    Ok("Update review number " + id)
+  def delete(id: Int): Action[AnyContent] = Action {
+    reviewRepo.delete(id)
+    Redirect("/reviews/all")
   }
 
-  def updateHandle = Action {
-    Ok("Handle update review")
+  def update(id: Int): Action[AnyContent] = Action.async { implicit request: MessagesRequest[AnyContent] =>
+    reviewRepo.details(id).map {
+      case Some(r) => Ok(views.html.review.update(updateReviewForm.fill(UpdateReviewForm(r.id, r.product_id, r.description))))
+      case None => Redirect("/reviews/all")
+    }
   }
 
+  def updateHandle(): Action[AnyContent] = Action.async { implicit request =>
+    updateReviewForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest(views.html.review.update(errorForm))
+        )
+      },
+      review => {
+        reviewRepo.update(review.id, Review(review.id, review.product_id, review.description)).map { _ =>
+          Redirect(routes.ReviewController.update(review.id: Int)).flashing("success" -> "Review updated")
+        }
+      }
+    )
+  }
 }
+
+case class CreateReviewForm(product_id: Int, description: String)
+case class UpdateReviewForm(id: Int, product_id: Int, description: String)
