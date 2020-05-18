97dd2d3c268f9fd4cc43bbbe7b57cc8190e53bc1
nuxie <zkalinowska96@icloud.com>
2020-05-18 02:10:32 +0200

    Implement Stock controller methods

--- app/controllers/StockController.scala
+++ app/controllers/StockController.scala
@@ -1,25 +1,89 @@
 package controllers
 
 import javax.inject._
+import models.{Stock, StockRepository}
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
 
+import scala.concurrent.{ExecutionContext, Future}
+
 @Singleton
-class StockController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class StockController @Inject()(cc: MessagesControllerComponents, stockRepo: StockRepository)
+                                (implicit ec: ExecutionContext) extends MessagesAbstractController(cc) {
 
-  def list = Action {
-    Ok("All stock items")
+  val stockForm: Form[CreateStockForm] = Form {
+    mapping(
+      "product_id" -> number,
+      "quantity" -> number
+    )(CreateStockForm.apply)(CreateStockForm.unapply)
+  }
+  val updateStockForm: Form[UpdateStockForm] = Form {
+    mapping(
+      "id" -> number,
+      "product_id" -> number,
+      "quantity" -> number
+    )(UpdateStockForm.apply)(UpdateStockForm.unapply)
   }
 
-  def details(id: Int) = Action {
-    Ok("Details of stock item")
+  def add: Action[AnyContent] = Action { implicit request =>
+    Ok(views.html.stock.add(stockForm))
   }
 
-  def update(id: Int) = Action {
-    Ok("Update stock item number " + id)
+  def addHandle(): Action[AnyContent] = Action.async { implicit request =>
+    stockForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest(views.html.stock.add(errorForm))
+        )
+      },
+      stock => {
+        stockRepo.add(stock.product_id, stock.quantity).map { _ =>
+          Redirect(routes.StockController.add()).flashing("success" -> "Stock added")
+        }
+      }
+    )
   }
 
-  def updateHandle = Action {
-    Ok("Handle update stock item")
+  def list: Action[AnyContent] = Action.async { implicit request =>
+    stockRepo.list().map(s => Ok(views.html.stock.list(s)))
   }
 
+  def details(id: Int): Action[AnyContent] = Action.async { implicit request =>
+    val st: Future[Option[Stock]] = stockRepo.details(id)
+    st.map {
+      case Some(s) => Ok(views.html.stock.details(s))
+      case None => Redirect("/stocks/all")
+    }
+  }
+
+  def delete(id: Int): Action[AnyContent] = Action {
+    stockRepo.delete(id)
+    Redirect("/stocks/all")
+  }
+
+  def update(id: Int): Action[AnyContent] = Action.async { implicit request: MessagesRequest[AnyContent] =>
+    stockRepo.details(id).map {
+      case Some(s) => Ok(views.html.stock.update(updateStockForm.fill(UpdateStockForm(s.id, s.product_id, s.quantity))))
+      case None => Redirect("/stocks/all")
+    }
+  }
+
+  def updateHandle(): Action[AnyContent] = Action.async { implicit request =>
+    updateStockForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest(views.html.stock.update(errorForm))
+        )
+      },
+      stock => {
+        stockRepo.update(stock.id, Stock(stock.id, stock.product_id, stock.quantity)).map { _ =>
+          Redirect(routes.StockController.update(stock.id: Int)).flashing("success" -> "Stock updated")
+        }
+      }
+    )
+  }
 }
+
+case class CreateStockForm(product_id: Int, quantity: Int)
+case class UpdateStockForm(id: Int, product_id: Int, quantity: Int)
