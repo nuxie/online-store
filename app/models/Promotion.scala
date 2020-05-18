cded77659144aac8f42d7ab170c4589b252af39c
nuxie <zkalinowska96@icloud.com>
2020-05-18 02:15:07 +0200

    Change flag_active to Int

--- app/models/Promotion.scala
+++ app/models/Promotion.scala
@@ -3,7 +3,7 @@ package models
 import play.api.libs.json.Json
 import play.api.libs.json.OFormat
 
-case class Promotion(id: Int, name: String, flag_active: Boolean, product_id: Int, percentage_sale: Int)
+case class Promotion(id: Int, name: String, flag_active: Int, product_id: Int, percentage_sale: Int)
 
 object Promotion {
   implicit val promotionFormat: OFormat[Promotion] = Json.format[Promotion]
