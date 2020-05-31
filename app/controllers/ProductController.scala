778af94356670df69ec9f693f7af246c50ee95d5
nuxie <zkalinowska96@icloud.com>
2020-06-01 00:24:33 +0200

    Implement product REST API

--- app/controllers/ProductController.scala
+++ app/controllers/ProductController.scala
@@ -4,6 +4,7 @@ import javax.inject._
 import models.{Product, ProductRepository}
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -87,6 +88,51 @@ class ProductController @Inject()(cc: MessagesControllerComponents, productRepo:
         }
       }
     )
+  }
+
+  def listJSON: Action[AnyContent] = Action.async { implicit request =>
+    productRepo.list().map(p =>
+      Ok(Json.toJson(p))
+    )
+  }
+
+  def detailsJSON(id: Int): Action[AnyContent] = Action.async { implicit request =>
+    productRepo.details(id).map {
+      case Some(p) => Ok(Json.toJson(p))
+      case None => NotFound(Json.obj(
+        "status" -> "Error",
+        "message" -> "Not found"
+      ))
+    }
+  }
+
+  def addJSON(): Action[JsValue] = Action(parse.json) { request =>
+    request.body.validate[Product].fold({ errors =>
+      BadRequest(Json.obj(
+        "status" -> "Error",
+        "message" -> "Bad JSON"
+      ))
+    }, { product =>
+      productRepo.add(product.name, product.description, product.category_id, product.price)
+      Ok(Json.obj("status" -> "OK", "message" -> "Product created"))
+    })
+  }
+
+  def updateJSON(id: Int): Action[JsValue] = Action(parse.json) {  request =>
+    request.body.validate[Product].fold({ errors =>
+      BadRequest(Json.obj(
+        "status" -> "Error",
+        "message" -> "Bad JSON"
+      ))
+    }, { product =>
+      productRepo.update(id, product)
+      Ok(Json.obj("status" -> "OK", "message" -> "Product updated"))
+    })
+  }
+
+  def deleteJSON(id: Int): Action[JsValue] = Action(parse.json) {  request =>
+    productRepo.delete(id)
+    Ok(Json.obj("status" -> "OK", "message" -> "Product deleted"))
   }
 }
 
