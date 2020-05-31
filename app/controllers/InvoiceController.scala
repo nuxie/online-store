22e43c774cf7ee0e86c1cfb71119404889ea1c6b
nuxie <zkalinowska96@icloud.com>
2020-06-01 00:17:51 +0200

    Implement invoices REST API

--- app/controllers/InvoiceController.scala
+++ app/controllers/InvoiceController.scala
@@ -4,6 +4,7 @@ import javax.inject._
 import models.{Invoice, InvoiceRepository}
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -82,6 +83,51 @@ class InvoiceController @Inject()(cc: MessagesControllerComponents, invoiceRepo:
         }
       }
     )
+  }
+
+  def listJSON: Action[AnyContent] = Action.async { implicit request =>
+    invoiceRepo.list().map(p =>
+      Ok(Json.toJson(p))
+    )
+  }
+
+  def detailsJSON(id: Int): Action[AnyContent] = Action.async { implicit request =>
+    invoiceRepo.details(id).map {
+      case Some(p) => Ok(Json.toJson(p))
+      case None => NotFound(Json.obj(
+        "status" -> "Error",
+        "message" -> "Not found"
+      ))
+    }
+  }
+
+  def createJSON(): Action[JsValue] = Action(parse.json) { request =>
+    request.body.validate[Invoice].fold({ errors =>
+      BadRequest(Json.obj(
+        "status" -> "Error",
+        "message" -> "Bad JSON"
+      ))
+    }, { invoice =>
+      invoiceRepo.create(invoice.order_id, invoice.payment_due)
+      Ok(Json.obj("status" -> "OK", "message" -> "Invoice created"))
+    })
+  }
+
+  def updateJSON(id: Int): Action[JsValue] = Action(parse.json) {  request =>
+    request.body.validate[Invoice].fold({ errors =>
+      BadRequest(Json.obj(
+        "status" -> "Error",
+        "message" -> "Bad JSON"
+      ))
+    }, { invoice =>
+      invoiceRepo.update(id, invoice)
+      Ok(Json.obj("status" -> "OK", "message" -> "Invoice updated"))
+    })
+  }
+
+  def deleteJSON(id: Int): Action[JsValue] = Action(parse.json) {  request =>
+    invoiceRepo.delete(id)
+    Ok(Json.obj("status" -> "OK", "message" -> "Invoice deleted"))
   }
 }
 
